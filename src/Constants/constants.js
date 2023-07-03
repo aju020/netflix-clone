80ef1cd4bfa1b2ca554ed08be7d063650ef9babd
AJAY V <90496625+aju020@users.noreply.github.com>
2023-07-03 06:15:13 +0530

    Update constants.js

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,3 +1,3 @@
 export const baseURL = "https://api.themoviedb.org/3";
-export const API_KEY="YOUR_API_KEY"
+export const API_KEY="c17c54de5030ec8c3d0ad2a69146ec52"
 export const imageURL = "https://image.tmdb.org/t/p/original"
